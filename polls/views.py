a56fca2ffb32cf85a217322c373caaf79d11ef4c
javier <223222@ids.upchiapas.edu.mx>
2023-03-08 11:19:58 -0600

    agregar los nuevos templates

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from .models import Article
+# Create your views here.
+
 def index(request):
     return HttpResponse("hello")
